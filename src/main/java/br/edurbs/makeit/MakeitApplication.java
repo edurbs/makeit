8d4bf8e67c2aa83991be149d380e3a476bec0ec8
Eduardo Soares <edurbs@gmail.com>
2024-01-10 19:22:54 -0300

    first commit

---
+++ src/main/java/br/edurbs/makeit/MakeitApplication.java
@@ -0,0 +1,13 @@
+package br.edurbs.makeit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakeitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MakeitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edurbs/makeit/MakeitApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edurbs.makeit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakeitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
