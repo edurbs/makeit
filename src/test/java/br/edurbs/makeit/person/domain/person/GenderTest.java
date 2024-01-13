77af03e73bfcfbc0813cc3a18e65a5da80095d8a
Eduardo Soares <edurbs@gmail.com>
2024-01-13 14:37:00 -0300

    add gender tests

---
+++ src/test/java/br/edurbs/makeit/person/domain/person/GenderTest.java
@@ -0,0 +1,13 @@
+package br.edurbs.makeit.person.domain.person;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+class GenderTest {
+    @Test
+    void getName_returnsCorrectValue() {
+        Gender male = Gender.MALE;
+
+        assertEquals("Masculino", male.getName());
+    }
+}
